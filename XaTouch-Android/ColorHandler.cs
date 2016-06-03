f2b077352a903ab05dc517bd8df991cb5e0e2f67
sibinet <sibi.nambiar@live.com>
2016-06-03 19:38:56 +0400

    Add files via upload

---
+++ XaTouch-Android/ColorHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Xml;
+
+namespace testXaDroid
+{
+	public class ColorHandler
+	{
+		public static  ColorPattern FetchColor ()
+		{
+			    Random randonGen = new Random ();
+
+			     ColorPattern ReturnColorPattern = new ColorPattern ();
+					
+			     ReturnColorPattern.R = randonGen.Next (256);
+			     ReturnColorPattern.G = randonGen.Next (256);
+			     ReturnColorPattern.B = randonGen.Next (256);
+
+                 return ReturnColorPattern;
+		}
+	}
+	public class ColorPattern
+	{
+		public int R { get; set; }
+		public int G { get; set; }
+		public int B { get; set; }
+	}
+}
+
